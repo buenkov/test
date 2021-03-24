088297600e6bef6c3a18b50a032c91d6280d1c0d
buenkov <buenkov-ta@yandex.ru>
2021-03-24 15:07:51 +0300

    all branch delete

--- switch/src/test_branch.java
+++ switch/src/test_branch.java
@@ -1,9 +1,7 @@
 public class test_branch {
     public static void main(String[] args) {
-<<<<<<<<< Temporary merge branch 1
-        System.out.println("branch1");
-=========
+
         System.out.println("branch3");
->>>>>>>>> Temporary merge branch 2
+
     }
 }
